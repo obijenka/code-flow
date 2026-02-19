6bacf09f6bce7dff84922e0bacb1c04bf4dee1bd
obijenka <annasamailovich@gmail.com>
2026-02-19 17:27:33 +0300

    git commit -m "Initial commit"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Features } from "tailwindcss"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+
+      
+    </div>
+  )
+}
+
+export default App
